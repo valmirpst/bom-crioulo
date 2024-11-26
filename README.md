75a85109fd96b96e0e2e77d872fef6e1da80b7ac
Valmir Paiva Stachin <valmirpaivacpm@gmail.com>
2024-11-26 12:53:15 -0300

    feat: configura o ambiente do projeto

---
+++ README.md
@@ -0,0 +1,11 @@
+# Website do Livro Bom Crioulo
+
+## Trabalho de Português - M33 2024
+
+### Alunos
+
+- Amanda Soares Vieira
+- Lara Deitos
+- Matheus Teodoro Garcia
+- Natália Campos Soares
+- Valmir Paiva Stachin
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './index.css';
+import Home from './pages/home';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
